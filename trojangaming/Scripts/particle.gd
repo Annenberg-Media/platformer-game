09c2e0dfe41290a5303756b520d08dffa548cc3f
Steven R. Xie <48636116+khiro-ptera@users.noreply.github.com>
2025-03-18 20:40:46 -0700

    hurt particles

--- trojangaming/Scripts/particle.gd
+++ trojangaming/Scripts/particle.gd
@@ -2,9 +2,10 @@ extends RigidBody2D
 
 var expire = 4.0
 var anim = "big"
+var offset = Vector2(randi_range(-16, 16), 28)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	global_position = get_parent().global_position + Vector2(randi_range(-16, 16), 28)
+	global_position = get_parent().global_position + offset
 	linear_velocity += Vector2(randi_range(-25, 25), -100)
 	$AnimatedSprite2D.play(anim)
 
